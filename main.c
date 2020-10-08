28fbd151a8b0754ae26805f50f895f47a4e096ad
suyeonida <70572398+suyeonida@users.noreply.github.com>
2020-10-08 16:51:52 +0900

    04

--- main.c
+++ main.c
@@ -3,17 +3,46 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
- int square(int a){
- 	return(a*a);
- }
+//팩토리얼 계산함수  
+int factorial(int n){
 	
-int main(int argc, char *argv[]) {
+	int i;
+	int factorial=1;
+	for(i=1;i<=n;i++){
+		factorial= factorial*i;
+	}
+		return factorial;
+}
+//정수 받는 함수
+int get_number(){
+		
+	int x;
+	printf("input the number: ");
+	scanf("%d", &x);
+	return x;
+	} 
+//최종계산함수 (combination)
+int combination(int n, int r){
+	int a,b,c;
+	a=factorial(n);
+	b=factorial(n-r);
+	c=factorial(r);
 	
-	int a=2;
-	a=square(a);
-	printf("a=%i\n",a);
-	 
-
+	return a/(b*c);
+}
+ int main(int argc, char *argv[]) {
+	
+	int n,r, output;
+	n=get_number();
+	r=get_number();
+	
+	output= combination(n,r);
+	
+	printf("The result is %d",output);
+	
+	return 0;
+	
+	
 }
 
 
