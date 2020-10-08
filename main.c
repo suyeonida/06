21be437345fe9c5f9f20170eb3dc56008e5b46d7
suyeonida <70572398+suyeonida@users.noreply.github.com>
2020-10-08 16:02:18 +0900

    02

--- main.c
+++ main.c
@@ -3,20 +3,27 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int star(){
-	
-	int i;
-	
-	for(i=0; i<10; i++){
-		printf("*");
-	}
+int sumTwo(int a, int b){
+	return (a+b);
 }
 
+int square(int n){
+	return (n*n);
+}
+
+int get_max(int x, int y){
+	if(x>y)
+		return x;
+	else
+		return y;
+}
+	//아래 보시면 원인을 알려줍니다 suqare라는 오타가 있네요   
 int main(int argc, char *argv[]) {
 	
-	star();
-	star();
-	star();
+	printf("sumTwo :%i\n", sumTwo(1,2));
+	printf("square :%i\n", square(5)); 
+	printf("get_max :%i\n", get_max(10,30));
+
 	return 0;
 
 }
